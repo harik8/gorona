3982a116b29990f03d5a5df560084e541b197ac1
Pieter Claerhout <pieter@yellowduck.be>
2020-03-25 07:55:57 +0100

    Changed the parsing to unmarshal the json directly
    It’s faster and save a lot of code…

--- cmd/root.go
+++ cmd/root.go
@@ -2,31 +2,31 @@ package cmd
 
 import (
 	"os"
-	"github.com/spf13/cobra"
+
 	cli "github.com/harik8/gorona/client"
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "gorona",
 	Short: "gorona cli",
-	Long: ` gorona | gorona <country>`,
+	Long:  "gorona | gorona <country>",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			cli.GetCountries()
 		} else if len(args) == 1 {
 			cli.GetCountry(args[0])
 		} else {
-			err := cmd.Help()
-			if err != nil {
+			if err := cmd.Help(); err != nil {
 				os.Exit(1)
 			}
 		}
 	},
-  }
-  
-  // Execute : cmd Execute
-  func Execute() {
+}
+
+// Execute : cmd Execute
+func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-	  os.Exit(1)
+		os.Exit(1)
 	}
-  }
+}
--- gorona/gorona.go
+++ gorona/gorona.go
@@ -2,13 +2,13 @@ package gorona
 
 // CaseState : Struct Case State
 type CaseState struct {
-	Country string `header:"Country"`
-	Cases float64 `header:"Cases"`
-	TodayCases float64 `header:"Today Cases"`
-	Deaths float64 `header:"Deaths"`
-	TodayDeaths float64 `header:"Today Deaths"`
-	Recovered float64 `header:"Recovered"`
-	Active float64 `header:"Active"`
-	Critical float64 `header:"Critical"`
-	CasesPerOneMillion float64 `header:"Cases Per One Million"`
+	Country            string `json:"country" header:"Country"`
+	Cases              int64  `json:"cases" header:"Cases"`
+	TodayCases         int64  `json:"todayCases" header:"Today Cases"`
+	Deaths             int64  `json:"deaths" header:"Deaths"`
+	TodayDeaths        int64  `json:"todayDeaths" header:"Today Deaths"`
+	Recovered          int64  `json:"recovered" header:"Recovered"`
+	Active             int64  `json:"active" header:"Active"`
+	Critical           int64  `json:"critical" header:"Critical"`
+	CasesPerOneMillion int64  `json:"casesPerOneMillion" header:"Cases Per One Million"`
 }
